940623dea58e125deadde4b52ade3f8d7fd175ba
unknown <GIHristov24@codingburgas.bg>
2026-05-17 22:46:17 +0300

    Add header files

---
+++ HardCoders/include/constants.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// Path to the data file
+static const char* DATA_FILE = "data/books.txt";
+
+// Year range for validation
+static const int MIN_YEAR = 1000;
+static const int MAX_YEAR = 2100;
